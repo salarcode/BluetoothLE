0e3e949bf8f100b4626f41dc5a9e2baa2fe51e00
salarcode <salar2k@gmail.com>
2026-03-26 23:03:26 +1100

    Reverting permission changes

--- src/Salar.BluetoothLE/Android/AndroidPermissions.cs
+++ src/Salar.BluetoothLE/Android/AndroidPermissions.cs
@@ -2,10 +2,11 @@ using Android.App;
 
 // Note: copy this file to `/Platforms/Android/AndroidPermissions.cs` in your app.
 
-[assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation, MaxSdkVersion = 30)]
-[assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation, MaxSdkVersion = 30)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
 
 [assembly: UsesPermission(Android.Manifest.Permission.Bluetooth, MaxSdkVersion = 30)]
 [assembly: UsesPermission(Android.Manifest.Permission.BluetoothAdmin, MaxSdkVersion = 30)]
 [assembly: UsesPermission(Android.Manifest.Permission.BluetoothScan)]
 [assembly: UsesPermission(Android.Manifest.Permission.BluetoothConnect)]
+[assembly: UsesPermission(Android.Manifest.Permission.BluetoothPrivileged)]

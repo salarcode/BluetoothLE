49aae6a918250ddf7c571e33220fda0d5edb973d
salarcode <salar2k@gmail.com>
2026-03-26 23:27:47 +1100

    Avalonia

---
+++ samples/BleAvaloniaDemo/BleAvaloniaDemo/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace BleAvaloniaDemo.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ samples/BleAvaloniaDemo/BleAvaloniaDemo/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace BleAvaloniaDemo.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
